50b72d0c275811df1b77f6ec37dfbb0e5cf2c986
Kerem Gunes <qeremy@gmail.com>
2015-12-31 00:21:46 +0200

    complete SetUri().

--- src/http/request.go
+++ src/http/request.go
@@ -1,5 +1,9 @@
 package http
 
+import (
+    "util"
+)
+
 type Request struct {
     Stream
     method string
@@ -10,4 +14,15 @@ func NewRequest() (*Request) {
     return &Request{
         Stream: *NewStream(TYPE_REQUEST, HTTP_VERSION_1_0),
     }
+}
+
+func (this *Request) SetUri(uri string, uriParams map[string]string) (*Request) {
+    if uriParams != nil {
+        var query string
+        for key, value := range uriParams {
+            query += util.StringFormat("%s=%s", key, util.UrlEncode(value))
+        }
+        uri += "?"+ query
+    }
+    return this
 }
