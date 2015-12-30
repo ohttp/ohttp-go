c4fa4d549922c926cf8b8e702f6893517e9c509c
Kerem Gunes <qeremy@gmail.com>
2015-12-30 04:47:02 +0200

    change param name.

--- src/http/headers/headers.go
+++ src/http/headers/headers.go
@@ -8,12 +8,12 @@ func New() (*Headers) {
     return &Headers{}
 }
 
-func (this *Headers) Set(key, value string) (*Headers) {
-    this.data[key] = value
+func (this *Headers) Set(name, value string) (*Headers) {
+    this.data[name] = value
     return this
 }
-func (this *Headers) Get(key string) (string) {
-    if value, ok := this.data[key]; ok {
+func (this *Headers) Get(name string) (string) {
+    if value, ok := this.data[name]; ok {
         return value
     }
     return ""
