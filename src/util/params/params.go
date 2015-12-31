bea4e739ae3c32a93e9fd2f0244d986992879daf
Kerem Gunes <qeremy@gmail.com>
2015-12-31 22:16:34 +0200

    completely update util.params pkg.

--- src/util/params/params.go
+++ src/util/params/params.go
@@ -1,5 +1,30 @@
 package params
 
+import (
+    "util"
+)
+
 type Params map[string]interface{}
 
-type ParamsString map[string]string
+func New() (*Params) {
+    return &Params{}
+}
+func (this *Params) Set(k string, v interface{}) (*Params) {
+    (*this)[k] = v
+    return this
+}
+func (this *Params) Get(k string) (interface{}) {
+    return (*this)[k]
+}
+func (this *Params) GetInt(k string) (int) {
+    return util.Int((*this)[k])
+}
+func (this *Params) GetUInt(k string) (uint) {
+    return util.UInt((*this)[k])
+}
+func (this *Params) GetString(k string) (string) {
+    return util.String((*this)[k])
+}
+func (this *Params) GetBool(k string) (bool) {
+    return util.Bool((*this)[k])
+}
