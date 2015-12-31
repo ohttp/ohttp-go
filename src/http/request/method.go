747362d0b853b158812de03da2fee39f116a99a6
Kerem Gunes <qeremy@gmail.com>
2015-12-31 03:44:51 +0200

    add http.request.method pkg.

---
+++ src/http/request/method.go
@@ -0,0 +1,16 @@
+package request
+
+const (
+    OPTIONS = "OPTIONS"
+    HEAD    = "HEAD"
+    GET     = "GET"
+    POST    = "POST"
+    PUT     = "PUT"
+    PATCH   = "PATCH"
+    DELETE  = "DELETE"
+    TRACE   = "TRACE"
+    CONNECT = "CONNECT"
+    // non-standard
+    COPY    = "COPY"
+    MOVE    = "MOVE"
+)
