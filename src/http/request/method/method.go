1448060b7703860247cac76a6e57d6d7d16f1b5c
Kerem Gunes <qeremy@gmail.com>
2015-12-31 07:12:48 +0200

    trivial.

--- src/http/request/method/method.go
+++ src/http/request/method/method.go
@@ -3,16 +3,16 @@ package method
 // HTTP methods.
 // @const string
 const (
-    OPTIONS = "OPTIONS"
-    HEAD    = "HEAD"
-    GET     = "GET"
-    POST    = "POST"
-    PUT     = "PUT"
-    PATCH   = "PATCH"
-    DELETE  = "DELETE"
-    TRACE   = "TRACE"
-    CONNECT = "CONNECT"
+    OPTIONS   = "OPTIONS"
+    HEAD      = "HEAD"
+    GET       = "GET"
+    POST      = "POST"
+    PUT       = "PUT"
+    PATCH     = "PATCH"
+    DELETE    = "DELETE"
+    TRACE     = "TRACE"
+    CONNECT   = "CONNECT"
     // non-standard
-    COPY    = "COPY"
-    MOVE    = "MOVE"
+    COPY      = "COPY"
+    MOVE      = "MOVE"
 )
