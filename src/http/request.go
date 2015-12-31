2a89a1f8a42e7c3d6922b6f1ab8bd0f8be89f0d5
Kerem Gunes <qeremy@gmail.com>
2015-12-31 05:08:56 +0200

    move http.request as package http.response.

--- src/http/request.go
+++ src/http/request.go
@@ -1,44 +1,15 @@
 package http
 
 import (
-    _str "strings"
+    "http/message"
+    "http/request"
 )
 
-import (
-    "util"
-)
-
-type Request struct {
-    Stream
-    method string
-    uri    *Uri
-}
-
-func NewRequest() (*Request) {
-    return &Request{
-        Stream: *NewStream(TYPE_REQUEST, HTTP_VERSION_1_0),
-    }
-}
-
-func (this *Request) SetMethod(method string) (*Request) {
-    this.method = _str.ToUpper(method)
-    return this
-}
-func (this *Request) GetMethod() (string) {
-    return this.method
-}
-
-func (this *Request) SetUri(uri string, uriParams map[string]string) (*Request) {
-    if uriParams != nil {
-        var query string
-        for key, value := range uriParams {
-            query += util.StringFormat("%s=%s", key, util.UrlEncode(value))
-        }
-        uri += "?"+ query
-    }
-    this.uri = NewUri(uri)
-    return this
-}
-func (this *Request) GetUri() (*Uri) {
-    return this.uri
+func NewRequest() (*request.Request) {
+    return request.New(
+        message.NewMessage(
+            message.TYPE_REQUEST,
+            message.PROTOCOL_VERSION_1_0,
+        ),
+    )
 }
