063d6497fbc700fd68c306f173934f3d22d757aa
Kerem Gunes <qeremy@gmail.com>
2016-01-01 08:07:51 +0200

    add Send().

--- src/http/request/request.go
+++ src/http/request/request.go
@@ -1,6 +1,12 @@
 package request
 
 import (
+    _fmt "fmt"
+    _net "net"
+    _bio "bufio"
+)
+
+import (
     "util"
     "http/message"
     "http/uri"
@@ -33,4 +39,71 @@ func (this *Request) GetMethod() (string) {
 }
 func (this *Request) GetUri() (*uri.Uri) {
     return this.uri
+}
+
+func (this *Request) Send() (string, error) {
+    this.SetHeader("Host", this.uri.Host())
+    this.SetHeader("Connection", "close")
+
+    uh := this.uri.Host()
+    if s := this.uri.Scheme(); s != "" {
+        if s == "http" {
+            // dial via http
+        } else if s == "https" {
+            // dial via https
+        } else {
+            // ...
+        }
+    }
+
+    // link, err := _net.Dial("tcp", uh)
+    // if err != nil {
+    //     panic(err)
+    // }
+    defer link.Close()
+
+    us := "/"
+    if s := this.uri.Path(); s != "" {
+        us = s
+    }
+    if s := this.uri.Query().String(); s != "" {
+        us += "?"+ s
+    }
+
+    var rs, rr string
+    rs += _fmt.Sprintf("%s %s HTTP/%s\r\n", this.method, us, this.GetProtocolVersion())
+    for k, v := range this.GetHeaderAll() {
+        if v != "" {
+            rs += _fmt.Sprintf("%s: %s\r\n", k, v)
+        }
+    }
+    rs += "\r\n"
+    rs += this.GetBody().Content()
+    util.Dumps(rs)
+
+    _fmt.Fprint(link, rs)
+
+    r := _bio.NewReader(link)
+    // status-line
+    s, err := r.ReadString('\n')
+    if s == "" {
+        print("HTTP error: no response returned from server!\n")
+        print("---------------------------------------------\n")
+        print(rs)
+        print("---------------------------------------------\n")
+        panic(err)
+    }
+    rr += s
+
+    for {
+        b := make([]byte, 1024)
+        if bl, _ := r.Read(b); bl == 0 {
+            break // eof
+        }
+        rr += util.Trim(string(b), "\x00")
+    }
+
+    link.Close()
+
+    return rr, nil
 }
