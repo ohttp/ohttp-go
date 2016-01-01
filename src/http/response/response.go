a916d31b32b3a76acdb0772d49eeecf3f56d3590
Kerem Gunes <qeremy@gmail.com>
2016-01-02 00:28:36 +0200

    remove unneeden status stuff's.

--- src/http/response/response.go
+++ src/http/response/response.go
@@ -27,27 +27,12 @@ func (this *Response) SetStatus(s string) (*Response) {
     if len(m) == 3 {
         this.status.SetTextPhrase(s)
         c, t := util.Int(m[1]), util.Trim(m[2], "")
-        this.SetStatusCode(c)
-        this.SetStatusText(t)
+        this.status.SetCode(c)
+        this.status.SetText(t)
     }
     return this
 }
-func (this *Response) SetStatusCode(c int) (*Response) {
-    this.status.SetCode(c)
-    return this
-}
-func (this *Response) SetStatusText(t string) (*Response) {
-    this.status.SetText(t)
-    return this
-}
-
 
 func (this *Response) GetStatus() (*status.Status) {
     return this.status
-}
-func (this *Response) GetStatusCode() (int) {
-    return this.status.GetCode()
-}
-func (this *Response) GetStatusText() (string) {
-    return this.status.GetText()
 }
