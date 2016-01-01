0584489d7cac297109b6f724abee5597779e8ab8
Kerem Gunes <qeremy@gmail.com>
2016-01-01 04:34:11 +0200

    remove tmp stuff.

--- src/util/query/query.go
+++ src/util/query/query.go
@@ -40,35 +40,3 @@ func (this *Query) Get(k string) (interface{}) {
 func (this *Query) Params() (*params.Params) {
     return this.params
 }
-
-/*func (this *Query) DataString() (string) {
-    if this.dataString != "" {
-        return this.dataString
-    }
-
-    for k, v := range this.data {
-        vt := util.TypeReal(v)
-        if vt == "[]int" {
-            v = _fmt.Sprintf("[\"%s\"]", _str.Join(v.([]int), "\",\""))
-        } else if vt == "[]string" {
-            v = _fmt.Sprintf("[\"%s\"]", _str.Join(v.([]string), "\",\""))
-        }
-        this.dataString += _fmt.Sprintf(
-            "%s=%s&", util.UrlEncode(k), util.UrlEncode(util.String(v)))
-    }
-
-    if this.dataString != "" {
-        // drop last "&"
-        this.dataString = this.dataString[0 : len(this.dataString) - 1]
-        // purify some encoded stuff
-        this.dataString = _str.NewReplacer(
-            "%5B", "[",
-            "%5D", "]",
-            "%2C", ",",
-        ).Replace(this.dataString)
-    }
-
-    return this.dataString
-}
-
-*/
