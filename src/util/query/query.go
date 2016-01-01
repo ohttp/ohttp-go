c7578e28c4169424a16c1b114d6e7dac8e287f96
Kerem Gunes <qeremy@gmail.com>
2016-01-01 04:48:23 +0200

    extend constructor, remove paramsString.

--- src/util/query/query.go
+++ src/util/query/query.go
@@ -1,18 +1,11 @@
 package query
 
 import (
-    // _fmt "fmt"
-    // _str "strings"
-)
-
-import (
-    // "util"
     "util/params"
 )
 
 type Query struct {
-    params        *params.Params
-    paramsString  string
+    params   *params.Params
 }
 
 func Shutup() {}
@@ -20,8 +13,16 @@ func Shutup() {}
 func New(p interface{}) (*Query) {
     this := &Query{}
     this.params = params.New()
-    if _, ok := p.(*params.Params); ok {
-        for k, v := range (*p.(*params.Params)) {
+    if _, ok := p.(params.Params); ok {
+        for k, v := range p.(params.Params) {
+            this.params.Set(k, v)
+        }
+    } else if _, ok := p.(*params.Params); ok {
+        for k, v := range *p.(*params.Params) {
+            this.params.Set(k, v)
+        }
+    } else if _, ok := p.(map[string]interface{}); ok {
+        for k, v := range p.(map[string]interface{}) {
             this.params.Set(k, v)
         }
     }
