c43c515e9652d5a0c030b0e49d90d4c108ccc645
Kerem Gunes <qeremy@gmail.com>
2016-01-02 21:44:14 +0200

    test Patch(), PatchFunc().

--- test/main.go
+++ test/main.go
@@ -77,6 +77,14 @@ func main() {
     //     util.Dump(res.String())
     // })
 
+    // r := c.Patch("http://localhost/foo.json", nil, nil, nil)
+    // util.Dumpf("%#v", r)
+    // c.PatchFunc("http://localhost/foo.json", nil, nil, nil,
+    //     func(req *request.Request, res *response.Response) {
+    //     util.Dump(req.String())
+    //     util.Dump(res.String())
+    // })
+
     // q := query.New(map[string]interface{}{"a": true, "b": 1})
     // util.Dumpf("%#v", q.String())
     // util.Dumpf("%#v", q.Params().String())
