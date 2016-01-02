fbcced0719a299a191e6adeaa88b3efe81c49c40
Kerem Gunes <qeremy@gmail.com>
2016-01-02 21:52:23 +0200

    test Trace(), TraceFunc().

--- test/main.go
+++ test/main.go
@@ -93,6 +93,14 @@ func main() {
     //     util.Dump(res.String())
     // })
 
+    // r := c.Trace("http://localhost/foo.json", nil, nil)
+    // util.Dumpf("%#v", r)
+    // c.TraceFunc("http://localhost/foo.json", nil, nil,
+    //     func(req *request.Request, res *response.Response) {
+    //     util.Dump(req.String())
+    //     util.Dump(res.String())
+    // })
+
     // q := query.New(map[string]interface{}{"a": true, "b": 1})
     // util.Dumpf("%#v", q.String())
     // util.Dumpf("%#v", q.Params().String())
