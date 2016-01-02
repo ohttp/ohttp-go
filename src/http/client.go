9b8a281b4dbed152cfb11fc301d2f28209207dcb
Kerem Gunes <qeremy@gmail.com>
2016-01-02 04:57:47 +0200

    complete Do().

--- src/http/client.go
+++ src/http/client.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+    "util"
     "http/request"
     "http/response"
+    "http/headers"
 )
 
 type Client struct {
@@ -17,7 +19,50 @@ func NewClient() (*Client) {
     }
 }
 
-func (this *Client) Request(uri string, uriParams interface{}, body interface{}, headers interface{}) {}
+func (this *Client) Request() (*request.Request) {
+    return this.request
+}
+func (this *Client) Response() (*response.Response) {
+    return this.response
+}
+
+func (this *Client) Do(u string, up interface{}, b interface{}, h interface{}) (*response.Response) {
+    m, _, err := util.RegExpMatch(u, "^([A-Z]+)\\s+(.+)")
+    if len(m) < 3 {
+        panic("Usage: <Method GET|POST...> <Scheme http|https>://<Host>/<Path>... !")
+    }
+    util.Dumps(m[1], err, len(m))
+    this.request.SetMethod(m[1])
+    this.request.SetUri(m[2], up)
+    this.request.SetBody(b)
+    this.request.SetHeaderAll(h)
+
+    rs, err := this.request.Send()
+    if err != nil {
+        panic(err)
+    }
+
+    rt := util.Explode(rs, "\r\n\r\n", 2)
+    if len(rt) != 2 {
+        panic("No valid response returned from server!")
+    }
+
+    rh := headers.Parse(rt[0])
+    if _, ok := rh["0"]; ok {
+        this.response.SetStatus(rh["0"])
+    }
+    this.response.SetHeaderAll(rh)
+    this.response.SetBody(rt[1])
+
+    if this.response.Status().Code() >= 400 {
+        this.response.SetError(
+            this.response.Status().Code(),
+            this.response.Status().Text(),
+        )
+    }
+
+    return this.response
+}
 
 func (this *Client) Head() {}
 func (this *Client) HeadFunc() {}
