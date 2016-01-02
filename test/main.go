ff12f417876bcb7a382b4637ffe43b92c7482cde
Kerem Gunes <qeremy@gmail.com>
2016-01-02 21:55:44 +0200

    test Copy(), CopyFunc().

--- test/main.go
+++ test/main.go
@@ -109,6 +109,14 @@ func main() {
     //     util.Dump(res.String())
     // })
 
+    // r := c.Copy("http://localhost/foo.json", nil, nil)
+    // util.Dumpf("%#v", r)
+    // c.CopyFunc("http://localhost/foo.json", nil, nil,
+    //     func(req *request.Request, res *response.Response) {
+    //     util.Dump(req.String())
+    //     util.Dump(res.String())
+    // })
+
     // q := query.New(map[string]interface{}{"a": true, "b": 1})
     // util.Dumpf("%#v", q.String())
     // util.Dumpf("%#v", q.Params().String())
