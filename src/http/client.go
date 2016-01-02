9d8348c7ddfd3f7354da66baaf7ee5707817b054
Kerem Gunes <qeremy@gmail.com>
2016-01-02 21:40:50 +0200

    shorter param types.

--- src/http/client.go
+++ src/http/client.go
@@ -30,7 +30,7 @@ func (this *Client) Response() (*response.Response) {
     return this.response
 }
 
-func (this *Client) Do(u string, up interface{}, b interface{}, h interface{}) (*response.Response) {
+func (this *Client) Do(u string, up, b, h interface{}) (*response.Response) {
     m, _, err := util.RegExpMatch(u, "^([A-Z]+)\\s+(.+)")
     if len(m) < 3 {
         panic("Usage: <Method GET|POST...> <Scheme http|https>://<Host>/<Path>... !")
@@ -65,43 +65,43 @@ func (this *Client) Do(u string, up interface{}, b interface{}, h interface{}) (
 
     return this.response
 }
-func (this *Client) DoFunc(u string, up interface{}, b interface{}, h interface{},
+func (this *Client) DoFunc(u string, up, b, h interface{},
     fn func (req *request.Request, res *response.Response)) {
     this.Do(u, up, b, h)
     fn(this.request, this.response)
 }
 
-func (this *Client) Options(u string, up interface{}, h interface{}) (*response.Response) {
+func (this *Client) Options(u string, up, h interface{}) (*response.Response) {
     return this.Do(method.OPTIONS +" "+ u, up, nil, h)
 }
-func (this *Client) OptionsFunc(u string, up interface{}, h interface{},
+func (this *Client) OptionsFunc(u string, up, h interface{},
     fn func (req *request.Request, res *response.Response)) {
     this.Options(u, up, h)
     fn(this.request, this.response)
 }
 
-func (this *Client) Head(u string, up interface{}, h interface{}) (*response.Response) {
+func (this *Client) Head(u string, up, h interface{}) (*response.Response) {
     return this.Do(method.HEAD +" "+ u, up, nil, h)
 }
-func (this *Client) HeadFunc(u string, up interface{}, h interface{},
+func (this *Client) HeadFunc(u string, up, h interface{},
     fn func (req *request.Request, res *response.Response)) {
     this.Head(u, up, h)
     fn(this.request, this.response)
 }
 
-func (this *Client) Get(u string, up interface{}, h interface{}) (*response.Response) {
+func (this *Client) Get(u string, up, h interface{}) (*response.Response) {
     return this.Do(method.GET +" "+ u, up, nil, h)
 }
-func (this *Client) GetFunc(u string, up interface{}, h interface{},
+func (this *Client) GetFunc(u string, up, h interface{},
     fn func (req *request.Request, res *response.Response)) {
     this.Get(u, up, h)
     fn(this.request, this.response)
 }
 
-func (this *Client) Post(u string, up interface{}, b interface{}, h interface{}) (*response.Response) {
+func (this *Client) Post(u string, up, b, h interface{}) (*response.Response) {
     return this.Do(method.POST +" "+ u, up, b, h)
 }
-func (this *Client) PostFunc(u string, up interface{}, b interface{}, h interface{},
+func (this *Client) PostFunc(u string, up, b, h interface{},
     fn func (req *request.Request, res *response.Response)) {
     this.Post(u, up, b, h)
     fn(this.request, this.response)
