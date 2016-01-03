6195e81f2b089ee7a7bd529413a113cca9bcc4ef
Kerem Gunes <qeremy@gmail.com>
2016-01-04 00:37:20 +0200

    add license & docs.

--- src/ohttp/util/array/sarray/sarray.go
+++ src/ohttp/util/array/sarray/sarray.go
@@ -1,6 +1,33 @@
-// @link https://gobyexample.com/collection-functions
+// Copyright (c) 2015-2016 Kerem Güneş
+//   <http://qeremy.com>
+//
+// GNU General Public License v3.0
+//   <http://www.gnu.org/licenses/gpl-3.0.txt>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+// @package    ohttp
+// @subpackage ohttp.util.array.iarray
+// @link       https://gobyexample.com/collection-functions
+// @author     Kerem Güneş <qeremy[at]gmail[dot]com>
 package sarray
 
+// Index.
+//
+// @param  arr []string
+// @param  s   string
+// @return (int)
 func Index(arr []string, is string) (int) {
     for i, v := range arr {
         if v == is {
@@ -10,10 +37,20 @@ func Index(arr []string, is string) (int) {
     return -1
 }
 
+// Has.
+//
+// @param  arr []string
+// @param  s   string
+// @return (bool)
 func Has(arr []string, is string) (bool) {
     return Index(arr, is) > -1
 }
 
+// Find.
+//
+// @param  arr []string
+// @param  s   string
+// @return (string, bool)
 func Find(arr []string, is string) (string, bool) {
     if i := Index(arr, is); i > -1 {
         return arr[i], true
@@ -21,6 +58,11 @@ func Find(arr []string, is string) (string, bool) {
     return "", false
 }
 
+// Find by index.
+//
+// @param  arr []string
+// @param  is  int
+// @return (string, bool)
 func FindIndex(arr []string, is int) (string, bool) {
     for i, v := range arr {
         if i == is {
@@ -30,6 +72,11 @@ func FindIndex(arr []string, is int) (string, bool) {
     return "", false
 }
 
+// Map.
+//
+// @param  arr []string
+// @param  fn  func(string, int) (string)
+// @return ([]string)
 func Map(arr []string, fun func(string, int) (string)) ([]string) {
     ret := make([]string, len(arr))
     for k, v := range arr {
@@ -38,6 +85,11 @@ func Map(arr []string, fun func(string, int) (string)) ([]string) {
     return ret
 }
 
+// Filter.
+//
+// @param  arr []string
+// @param  fn  func(string, int) (bool)
+// @return ([]string)
 func Filter(arr []string, fun func(string, int) (bool)) ([]string) {
     if fun == nil {
         fun = func(v string, k int) (bool) {
@@ -53,6 +105,10 @@ func Filter(arr []string, fun func(string, int) (bool)) ([]string) {
     return ret
 }
 
+// Uniq.
+//
+// @param  arr []string
+// @return ([]string)
 func Uniq(arr []string) ([]string) {
     ret, f := []string{}, map[string]bool{}
     for k := range arr {
@@ -64,6 +120,10 @@ func Uniq(arr []string) ([]string) {
     return ret
 }
 
+// Shift.
+//
+// @param  arr *[]string
+// @return (string)
 func Shift(arr *[]string) (string) {
     v := (*arr)[0]
     *arr = (*arr)[1 : len(*arr)]
