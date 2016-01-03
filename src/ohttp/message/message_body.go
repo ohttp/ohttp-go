164b577b801e658dd5b2fbfd6d7168862a8e842d
Kerem Gunes <qeremy@gmail.com>
2016-01-03 21:14:23 +0200

    update docs.

--- src/ohttp/message/message_body.go
+++ src/ohttp/message/message_body.go
@@ -17,8 +17,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-// @package ohttp.message
-// @author  Kerem Güneş <qeremy[at]gmail[dot]com>
+// @package    ohttp
+// @subpackage ohttp.message
+// @author     Kerem Güneş <qeremy[at]gmail[dot]com>
 package message
 
 // @object ohttp.message.MessageBody
