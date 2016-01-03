69151323362e22644d6e22229ec224a515180eab
Kerem Gunes <qeremy@gmail.com>
2016-01-03 21:04:55 +0200

    add license & docs.

--- src/ohttp/message/message_body_data.go
+++ src/ohttp/message/message_body_data.go
@@ -1,10 +1,37 @@
+// Copyright (c) 2015-2016 Kerem Güneş
+//   <http://qeremy.com>
+//
+// GNU General Public License v3.0
+//   <http://www.gnu.org/licenses/gpl-3.0.txt>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+// @package ohttp.message
+// @author  Kerem Güneş <qeremy[at]gmail[dot]com>
 package message
 
+// @object ohttp.method.MessageBodyData
 type MessageBodyData struct {
     content       interface{}
     contentType   string
 }
 
+// Constructor.
+//
+// @param  c  string
+// @param  ct string
+// @return (*ohttp.message.MessageBodyData)
 func NewMessageBodyData(c, ct string) (*MessageBodyData) {
     // @todo parse by contentType
     return &MessageBodyData{
@@ -13,6 +40,9 @@ func NewMessageBodyData(c, ct string) (*MessageBodyData) {
     }
 }
 
+// Get: content.
+//
+// @return (interface{})
 func (this *MessageBodyData) Content() (interface{}) {
     return this.content
 }
