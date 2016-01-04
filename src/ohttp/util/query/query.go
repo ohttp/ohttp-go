f19289d857803a4fb362803dae9935a84ca44911
Kerem Gunes <qeremy@gmail.com>
2016-01-04 03:08:49 +0200

    add license & docs.

--- src/ohttp/util/query/query.go
+++ src/ohttp/util/query/query.go
@@ -1,3 +1,26 @@
+// Copyright (c) 2015-2016 Kerem Güneş
+//   <http://qeremy.com>
+//
+// GNU General Public License v3.0
+//   <http://www.gnu.org/licenses/gpl-3.0.txt>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+// @package    ohttp
+// @subpackage ohttp.qeremy
+// @imports    ohttp.util, ohttp.util.params
+// @author     Kerem Güneş <qeremy[at]gmail[dot]com>
 package query
 
 import (
@@ -5,12 +28,17 @@ import (
     "ohttp/util/params"
 )
 
+// @object ohttp.query.Query
 type Query struct {
     params   *params.Params
 }
 
 func Shutup() {}
 
+// Constructor.
+//
+// @param  p interface{}
+// @return (*ohttp.qeremy.Query)
 func New(p interface{}) (*Query) {
     this := &Query{}
     this.params = params.New()
@@ -34,19 +62,34 @@ func New(p interface{}) (*Query) {
     return this
 }
 
+// Set.
+//
+// @param  k string
+// @param  v interface{}
+// @return (*ohttp.qeremy.Query)
 func (this *Query) Set(k string, v interface{}) (*Query) {
     this.params.Set(k, v)
     return this
 }
 
+// Get.
+//
+// @param  k string
+// @return v interface{}
 func (this *Query) Get(k string) (interface{}) {
     return this.params.Get(k)
 }
 
+// Get params object.
+//
+// @return (*ohttp.util.params.Params)
 func (this *Query) Params() (*params.Params) {
     return this.params
 }
 
+// Get as string.
+//
+// @return (string)
 func (this *Query) String() (string) {
     if this.params != nil {
         return this.params.String()
